727deb6cc1f041d67bae60b07f7c5a01ff459a32
Michael Caceres <michaeling10@hotmail.com>
2023-10-29 16:39:36 -0500

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+'use strict';
 
+import throttle from 'lodash.throttle';
+
+const loginForm = document.querySelector('.feedback-form');
+const emailInput = loginForm.elements.email;
+const messageTextarea = loginForm.elements.message;
+
+getInformationForm();
+
+loginForm.addEventListener('input', function (e) {
+  e.preventDefault();
+  saveInformationForm();
+});
+
+function saveInformationForm() {
+  const formInfo = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formInfo));
+}
+
+function getInformationForm() {
+  const formInfoSaved = localStorage.getItem('feedback-form-state');
+  if (formInfoSaved) {
+    const formInfo = JSON.parse(formInfoSaved);
+    emailInput.value = formInfo.email;
+    messageTextarea.value = formInfo.message;
+  }
+}
