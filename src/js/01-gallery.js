1dd7f761d2e223f9125f7d07aae32558cf4d18cb
Michael Caceres <michaeling10@hotmail.com>
2023-10-29 17:38:30 -0500

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,7 @@
 'use strict';
-// Add imports above this line
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
