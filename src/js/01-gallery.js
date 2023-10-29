31c6d7b9c6eac6eff7d7dffd9911f45b5fb275a5
Michael Caceres <michaeling10@hotmail.com>
2023-10-28 20:47:29 -0500

    Task 1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,46 @@
+'use strict';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemElements = [];
+
+galleryItems.forEach(item => {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.href = item.original;
+
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.src = item.preview;
+  galleryImage.alt = item.description;
+  galleryImage.title = galleryImage.alt;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+  galleryItemElements.push(galleryItem);
+});
+
+galleryContainer.append(...galleryItemElements);
+
+function handleKeyDown(e, instance) {
+  if (e.key === 'Escape') {
+    instance.close();
+    document.removeEventListener('keydown', handleKeyDown);
+  }
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  onShow: instance => {
+    document.addEventListener('keydown', e => handleKeyDown(e, instance));
+  },
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
